158ce44bd087df99cc76cb2fff44d7b603e97299
cos <3450414733@qq.com>
2022-03-10 23:42:31 +0800

    feat(DatePicker): 日期选择器

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 /*
  * @Author: cos
  * @Date: 2022-03-09 17:47:00
- * @LastEditTime: 2022-03-09 18:12:26
+ * @LastEditTime: 2022-03-10 20:42:30
  * @LastEditors: cos
  * @Description: 
- * @FilePath: \cosine-ui\src\App.js
+ * @FilePath: \HTML\project\cosine-ui\src\App.js
  */
 
 // src/App.js
 
+import moment from 'moment';
 import React, { Component } from 'react';
 import { DatePicker } from './components';
-
+import './App.scss'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <DatePicker />
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            nowDate: 'No Value!'
+        }
+    }
+    handleDateChange(date) {
+        this.setState({nowDate: date.format('YYYY-MM-DD')});
+    }
+    render() {
+        return (
+            <div className="App">
+                <div>nowDate: {this.state.nowDate}</div>
+                <DatePicker defaultDate={moment()} onDateChange={(date) => this.handleDateChange(date)} />
+            </div>
+        );
+    }
 }
 
 export default App;
